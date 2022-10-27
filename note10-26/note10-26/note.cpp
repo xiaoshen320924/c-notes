b497323e92ff236e37bf8c2bae67851e764b85cb
shenmengyang <smy320924@njfu.edu.cn>
2022-10-27 22:30:30 +0800

    显示替换

---
+++ note10-26/note10-26/note.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<string.h>
+//#include<windows.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	char a[] = "welcome to bit!";
+//	char b[] = "***************";
+//	int left = 0;
+//	int right = strlen(a) - 1;
+//	while (left <= right)
+//	{
+//		b[left] = a[left];
+//		b[right] = a[right];
+//		printf("%s\n", b);
+//		Sleep(1000);//睡眠函数，单位毫秒
+//		system("cls");//system是一个库函数，执行系统命令，cls是清空屏幕的意思
+//		left++;
+//		right--;
+//	}
+//	return 0;
+//}
