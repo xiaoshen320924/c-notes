ab3c5122ef166a39b6d56fdddb332929380be471
shenmengyang <smy320924@njfu.edu.cn>
2022-10-27 22:29:13 +0800

    goto语句

---
+++ note10-26/note10-26/noteGoto.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<Windows.h>
+//#include<stdlib.h>
+//#include<string.h>
+//int main()
+//{
+//	char input[20] = { 0 };
+//	system("shutdown -s -t 60");
+//again:
+//	printf("请注意，你的电脑将在一分钟内关机！请输入：我是猪  则取消关机\n");
+//	scanf("%s", input);
+//	if (0 == strcmp(input, "我是猪"))
+//	{
+//		printf("你说对了！这就取消换机");
+//		system("shutdown -a");
+//	}
+//		
+//	else
+//	{
+//		goto again;
+//	}
+//	return 0;
+//}
