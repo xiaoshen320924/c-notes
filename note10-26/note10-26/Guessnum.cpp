a4c47aface44d64c001eed76b33847fa03281007
shenmengyang <smy320924@njfu.edu.cn>
2022-10-29 11:14:13 +0800

    猜数游戏

--- note10-26/note10-26/Guessnum.cpp
+++ note10-26/note10-26/Guessnum.cpp
@@ -1,5 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 void menu()
 {
 	printf("***************\n");
@@ -7,9 +9,34 @@ void menu()
 	printf("*** 0.exit ****\n");
 	printf("***************\n");
 }
+void game()
+{
+	int i = 0, num = 0;
+	num = rand() % 100 + 1;
+
+	while (1)
+	{
+		printf("请猜一个数字：");
+		scanf("%d", &i);
+		if (i > num)
+		{
+			printf("猜大了,再试一次\n");
+		}
+		else if (i < num)
+		{
+			printf("猜小了，再试一次\n");
+		}
+		else
+		{
+			printf("恭喜猜对了！\n");
+			break;
+		}
+	}
+}
 int main()
 {
 	int input = 0;
+	srand((unsigned int)time(NULL));
 	do
 	{
 		menu();
@@ -18,6 +45,7 @@ int main()
 		switch (input)
 		{
 		case 1:
+			game();
 			break;
 		case 0:
 			break;
@@ -26,4 +54,5 @@ int main()
 			break;
 		}
 	} while (input);
+	return 0;
 }
