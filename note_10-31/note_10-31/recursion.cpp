e276d1b9f8417adf48f53b56f2b6e87d0f14a3ca
shenmengyang <smy320924@njfu.edu.cn>
2022-11-01 20:24:28 +0800

    返回a的k次方结果

---
+++ note_10-31/note_10-31/recursion.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int Power(int a,int b)
+//{
+//	if (b!=1)
+//	{
+//		return a * Power(a,b - 1);
+//	}
+//	else
+//	{
+//		return a;
+//	}
+//
+//}
+//
+//int main()
+//{
+//	int n = 0, k = 0;
+//	scanf("%d %d", &n, &k);
+//	int r=Power(n, k);
+//	printf("%d\n", r);
+//	return 0;
+//}
