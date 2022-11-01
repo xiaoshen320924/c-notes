8367695666dcf2b20acea443163bcc3107f8afa4
shenmengyang <smy320924@njfu.edu.cn>
2022-11-01 20:23:28 +0800

    输出每位数的和

---
+++ note_10-31/note_10-31/DigitSum.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int DigitSum(int n)
+//{
+//	if (n > 9)
+//	{
+//		return n % 10 + DigitSum(n / 10);
+//	}
+//	else
+//	{
+//		return n;
+//	}
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int r = DigitSum(a);
+//	printf("%d", r);
+//	return 0;
+//}
