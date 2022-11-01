2918b5b03e8b414bbe6c3e2e94bbc6ba91efdf5b
shenmengyang <smy320924@njfu.edu.cn>
2022-11-01 20:26:52 +0800

    递归方式实现strlen

---
+++ note_10-31/note_10-31/strlenr.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+////µ›πÈ µœ÷
+//int strlen(char* a)
+//{
+//	if (*a != '\0')
+//	{
+//		return 1 + strlen(a + 1);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	char s[] = "bit";
+//	int len = strlen(s);
+//	printf("%d\n", len);
+//	return 0;
+//}
